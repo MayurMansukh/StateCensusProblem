ec2b3691d06a528ab5b9459c9b5e1b0c939956a1
MayurMansukh <manojmansukh7004>
2021-03-23 17:38:21 +0530

    Added logic for throwing a custom if given a wrong file delimiter

--- src/test/java/com/State_Census_Analyser/StateCensusAnalyserTest.java
+++ src/test/java/com/State_Census_Analyser/StateCensusAnalyserTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class StateCensusAnalyserTest {
-    private StateCensusAnalyser stateCensusAnalyser;
-    private String path = "/Users/manojmansukh/Downloads/IndiaStateCensusData.csv";
-    private String Wrong_path = "/Users/manojmansukh/Downloads/IndiaStateCensusData.csv";
+    private StateCensusAnalyser stateCensusAnalyser;;
+    private String Correct_Path="/Users/manojmansukh/Downloads/IndiaStateCensusData.csv";
+    private String Wrong_Path="/Users/manojmansukh/Downloads/IndiaStateCensusData.csv";
+    private String Wrong_file_type="/Users/manojmansukh/Downloads/IndiaStateCensusData.csv";
+
 
     @BeforeAll
     public void setup() {
@@ -15,20 +17,39 @@ public class StateCensusAnalyserTest {
     }
 
     @Test
-    void checkDataIsLoaded() throws CustomException{
-        int size = stateCensusAnalyser.loadData(path);
-        System.out.println(size);
-        Assertions.assertEquals(30, size);
-
+    public void to_check_if_data_is_loaded() throws CustomException {
+        int numofEnteries = stateCensusAnalyser.loadData(Correct_Path);
+        Assertions.assertEquals(29, numofEnteries);
     }
 
     @Test
-    public void given_Wrong_file_path_should_return_custom_exception() throws CustomException {
-        try {
-            stateCensusAnalyser.loadData(Wrong_path);
-        } catch (CustomException e) {
-            throw new CustomException("Wrong file path");
+    public void given_Wrong_file_path_should_return_custom_exception() throws CustomException{
+        try{
+            stateCensusAnalyser.loadData(Wrong_Path);
+        }catch (CustomException e){
+            System.out.println(e.type);
+            Assertions.assertEquals(CustomException.ExceptionType.Wrong_File,e.type);
         }
-
     }
+
+    @Test
+    public void given_wrong_file_type_should_throw_custom_exception() throws CustomException{
+        try{
+            stateCensusAnalyser.loadData(Wrong_file_type);
+        }catch (CustomException e){
+            System.out.println(e.type);
+            Assertions.assertEquals(e.type,CustomException.ExceptionType.Wrong_File_Type);
+        }
+    }
+
+    @Test
+    public void given_wrong_delimiter_should_throw_custom_exception(){
+        try{
+            stateCensusAnalyser.loadData(Correct_Path);
+        }catch (CustomException e){
+            System.out.println(e.type);
+            Assertions.assertEquals(e.type, CustomException.ExceptionType.Wrong_File_Delimiter);
+        }
+    }
+
 }
